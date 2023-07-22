193d8c105c071227276b5eab6f62a9ba32ad4937
sor4chi <sorakawa0218@gmail.com>
2023-07-23 02:30:57 +0900

    feat: implement private endpoint handling

--- workers/gateway/src/index.ts
+++ workers/gateway/src/index.ts
@@ -7,5 +7,9 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/private/*', async (c) => {
+  const res = await c.env.PRIVATE_SERVICE.fetch(c.req.raw)
+  return res
+})
 
 export default app
--- workers/private-service/src/index.ts
+++ workers/private-service/src/index.ts
@@ -1,7 +1,7 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono().basePath("/private");
 
 app.get("/", (c) => c.text("Hello Private Service!"));
 
-export default app
+export default app;
